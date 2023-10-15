bbe1f899f6c83339607567c38f603ffb6f18b872
edwinmunoz416 <edwin.munoz416@gmail.com>
2023-10-15 02:11:07 -0500

    Typo fix

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>zendacti.</h1>
+      <h1>zencati.</h1>
       <p>"The Blue Sky must feel ashamed To Be So Small."</p>
     </main>
   )
